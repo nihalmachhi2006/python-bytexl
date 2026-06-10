62c8a32c737d97e7a6e2daa478a190cf49cf00b1
Nihal Machhi <nihalmachhi11@gmail.com>
2026-06-10 16:00:01 +0530

    Update main.py

--- Lecture-1/main.py
+++ Lecture-1/main.py
@@ -1,1 +1,19 @@
-print('hello world')
+class Solution:
+    def maxSubarraySum(self, arr, k):
+        # code here 
+        n=len(arr)
+        if(n<k):
+            return 0
+        window_sum = sum(arr[:k])
+        max_sum = window_sum
+        
+        for i in range(k,n):
+            window_sum += arr[i] - arr[i-k]
+            max_sum = max(max_sum,window_sum)
+        return max_sum
+
+# Max Sum Subarray of size K
+# Difficulty: EasyAccuracy: 49.6%Submissions: 293K+Points: 2
+# Given an array of integers arr[]  and a number k. Return the maximum sum of a subarray of size k.
+
+# Note: A subarray is a contiguous part of any given array.
