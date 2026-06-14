164e91d15893df8d1c1973103c9743faf6f2948e
Nihal Machhi <nihalmachhi11@gmail.com>
2026-06-14 19:49:57 +0530

    Implement search in rotated sorted array

--- Lecture-1/main.py
+++ Lecture-1/main.py
@@ -34,4 +34,40 @@ class Solution:
                 ans = nums[guess]
                 high = guess - 1
         return ans
+
+
+33. Search in Rotated Sorted Array
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        ans = -1
+        low = 0
+        high = len(nums) - 1
+
+        while(low <= high):
+            guess = (low + high) // 2
+            if(nums[guess] > nums[len(nums) - 1]):
+                if(nums[guess] == target):
+                    return guess
+                elif(nums[guess] < target):
+                    low = guess + 1 
+                else:
+                    if(nums[0] > target):
+                        low = guess + 1
+                    else:
+                        high = guess - 1
+            else:
+                if(nums[guess] == target):
+                    return guess
+                elif(nums[guess] < target):
+                    high = guess - 1
+                else:
+                    if(nums[len(nums) - 1] > target):
+                        high = guess - 1
+                    else:
+                        low = guess + 1
+        return ans
+
+
+        
         
