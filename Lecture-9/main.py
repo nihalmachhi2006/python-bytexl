56c85fbc45e7b5e7fa885144f599c5cb846da38d
Nihal Machhi <nihalmachhi11@gmail.com>
2026-06-18 08:46:47 +0530

    Implement level order traversal for binary tree

--- Lecture-9/main.py
+++ Lecture-9/main.py
@@ -1,1 +1,46 @@
 print('Lecture 9')
+
+
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+
+# level-oreder tranversal
+from queue import Queue
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        q = Queue()
+        q.put(root)
+
+        ans = []
+
+        while not q.empty():
+            level_size = q.qsize()
+            level = []
+
+            for _ in range(level_size):
+                node = q.get()
+                level.append(node.val)
+
+                if node.left:
+                    q.put(node.left)
+
+                if node.right:
+                    q.put(node.right)
+
+            ans.append(level)
+
+        return ans
