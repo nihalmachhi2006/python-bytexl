808036b121451a9753813691f8436b69993e02b4
Nihal Machhi <nihalmachhi11@gmail.com>
2026-07-07 13:57:55 +0530

    Add array subset problem description and examples
    Added problem statement and examples for array subset task.

--- Lecture-9/main.py
+++ Lecture-9/main.py
@@ -64,3 +64,59 @@ class Solution:
             return False
 
         return fun(p,q)
+
+
+
+# geeksforgeeks
+# Search...
+# Courses
+# Tutorials
+# Practice
+# Jobs
+
+# 99+
+# N
+
+
+# Array Subset
+# Difficulty: BasicAccuracy: 44.05%Submissions: 573K+Points: 1Average Time: 20m
+# Given two arrays a[] and b[], your task is to determine whether b[] is a subset of a[].
+
+# Examples:
+
+# Input: a[] = [11, 7, 1, 13, 21, 3, 7, 3], b[] = [11, 3, 7, 1, 7]
+# Output: true
+# Explanation: b[] is a subset of a[]
+# Input: a[] = [1, 2, 3, 4, 4, 5, 6], b[] = [1, 2, 4]
+# Output: true
+# Explanation: b[] is a subset of a[]
+# Input: a[] = [10, 5, 2, 23, 19], b[] = [19, 5, 3]
+# Output: false
+# Explanation: b[] is not a subset of a[]
+# Constraints:
+# 1 <= a.size(), b.size() <= 105
+# 1 <= a[i], b[j] <= 106
+
+
+
+class Solution {
+    public boolean isSubset(int a[], int b[]) {
+        // Your code here
+      
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for(int i=0 ;i < b.length; i++){
+            boolean flag = false;
+            for(int j=i ;j < a.length; j++){
+                if(b[i] == a[j]){
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag){
+                return false;
+            }
+        }
+        return true;
+    }
+}
