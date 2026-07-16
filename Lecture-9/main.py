99439429c809e834f671238dbd028755e7c4334e
Nihal Machhi <nihalmachhi11@gmail.com>
2026-07-16 14:11:01 +0530

    Update main.py

--- Lecture-9/main.py
+++ Lecture-9/main.py
@@ -120,3 +120,19 @@ class Solution {
         return true;
     }
 }
+ # transpost
+
+# class Solution {
+#     public int[][] transpose(int[][] matrix) {
+#     int n = matrix[0].length;
+#     int m = matrix.length;
+#     int ans[][] = new int[n][m];
+   
+#     for(int i = 0; i<matrix[0].length; i++){
+#         for(int j = 0; j<matrix.length; j++){
+#             ans[i][j] = matrix[j][i];
+#         }
+#      } 
+#      return ans;  
+#     }
+# }
