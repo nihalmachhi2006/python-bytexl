94fea39e0262f5a8e25991e6fd44ed972a071fe6
Nihal Machhi <nihalmachhi11@gmail.com>
2026-07-26 09:51:37 +0530

    Update main.py

--- Lecture-1/main.py
+++ Lecture-1/main.py
@@ -69,5 +69,33 @@ class Solution:
         return ans
 
 
+# Maximum Sum Subarray of Size K (easy)
+
+
+# class Solution {
+#     public int maxSubarraySum(int[] arr, int k) {
+#         // Code here
+#      int n = arr.length;
+#         if (n < k) return 0;
+
+#         int windowSum = 0;
+#         int maxSum = Integer.MIN_VALUE;
+
+#         // sum of first k elements
+#         for (int i = 0; i < k; i++) {
+#             windowSum += arr[i];
+#         }
+#         maxSum = windowSum;
+
+#         // slide the window
+#         for (int i = k; i < n; i++) {
+#             windowSum += arr[i] - arr[i - k];
+#             maxSum = Math.max(maxSum, windowSum);
+#         }
+
+#         return maxSum;
+#     }
+
+# }
         
         
